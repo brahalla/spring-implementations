a8d92d07b62ba4615d2a68e94ca0179d3cae2802
Daniel Cottone <daniel.cottone@gmail.com>
2016-02-28 10:55:02 -0600

    implementing querydsl

--- rest-api/src/main/java/com/brahalla/rest-api/repository/AlbumRepository.java
+++ rest-api/src/main/java/com/brahalla/rest-api/repository/AlbumRepository.java
@@ -4,15 +4,16 @@ import com.brahalla.restapi.domain.entity.Album;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(collectionResourceRel = "albums", path = "albums")
-public interface AlbumRepository extends PagingAndSortingRepository<Album, Long> {
+public interface AlbumRepository extends PagingAndSortingRepository<Album, Long>, QueryDslPredicateExecutor<Album> {
 
   @RestResource(path = "byName", rel = "byName")
-  Page<Album> findByName(@Param("name") String name, Pageable p);
+  Page<Album> findByNameContainingIgnoreCase(@Param("name") String name, Pageable p);
 
 }
--- rest-api/src/main/java/com/brahalla/rest-api/repository/PhotoRepository.java
+++ rest-api/src/main/java/com/brahalla/rest-api/repository/PhotoRepository.java
@@ -4,16 +4,20 @@ import com.brahalla.restapi.domain.entity.Photo;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import com.querydsl.core.types.dsl.StringPath;
+
 @RepositoryRestResource(collectionResourceRel = "photos", path = "photos")
-public interface PhotoRepository extends PagingAndSortingRepository<Photo, Long> {
+public interface PhotoRepository extends PagingAndSortingRepository<Photo, Long>, QueryDslPredicateExecutor<Photo> {
 
   @RestResource(path = "byName", rel = "byName")
-  Page<Photo> findByName(@Param("name") String name, Pageable p);
+  Page<Photo> findByNameContainingIgnoreCase(@Param("name") String name, Pageable p);
 
   @RestResource(path = "byAlbum", rel = "byAlbum")
   Page<Photo> findByAlbumId(@Param("id") Long albumId, Pageable p);
