8ab700a5a7f839393b5b0dde90dce7797067bd33
Daniel Cottone <daniel.cottone@egovtn.org>
2016-02-29 15:42:11 -0600

    cleaning up querydsl dependencies, adding support for customized bindings

--- rest-api/src/main/java/com/brahalla/rest-api/repository/AlbumRepository.java
+++ rest-api/src/main/java/com/brahalla/rest-api/repository/AlbumRepository.java
@@ -1,19 +1,29 @@
 package com.brahalla.restapi.repository;
 
 import com.brahalla.restapi.domain.entity.Album;
+import com.brahalla.restapi.domain.entity.QAlbum;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import com.querydsl.core.types.dsl.StringPath;
+
 @RepositoryRestResource(collectionResourceRel = "albums", path = "albums")
-public interface AlbumRepository extends PagingAndSortingRepository<Album, Long>, QueryDslPredicateExecutor<Album> {
+public interface AlbumRepository extends PagingAndSortingRepository<Album, Long>, QueryDslPredicateExecutor<Album>, QuerydslBinderCustomizer<QAlbum> {
 
   @RestResource(path = "byName", rel = "byName")
   Page<Album> findByNameContainingIgnoreCase(@Param("name") String name, Pageable p);
+
+  @Override
+  default void customize(QuerydslBindings bindings, QAlbum album) {
+		bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+	}
 
 }
--- rest-api/src/main/java/com/brahalla/rest-api/repository/PhotoRepository.java
+++ rest-api/src/main/java/com/brahalla/rest-api/repository/PhotoRepository.java
@@ -1,11 +1,13 @@
 package com.brahalla.restapi.repository;
 
 import com.brahalla.restapi.domain.entity.Photo;
+import com.brahalla.restapi.domain.entity.QPhoto;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -14,12 +16,17 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import com.querydsl.core.types.dsl.StringPath;
 
 @RepositoryRestResource(collectionResourceRel = "photos", path = "photos")
-public interface PhotoRepository extends PagingAndSortingRepository<Photo, Long>, QueryDslPredicateExecutor<Photo> {
+public interface PhotoRepository extends PagingAndSortingRepository<Photo, Long>, QueryDslPredicateExecutor<Photo>, QuerydslBinderCustomizer<QPhoto> {
 
   @RestResource(path = "byName", rel = "byName")
   Page<Photo> findByNameContainingIgnoreCase(@Param("name") String name, Pageable p);
 
   @RestResource(path = "byAlbum", rel = "byAlbum")
   Page<Photo> findByAlbumId(@Param("id") Long albumId, Pageable p);
+
+  @Override
+  default void customize(QuerydslBindings bindings, QPhoto photo) {
+		bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+	}
 
 }
