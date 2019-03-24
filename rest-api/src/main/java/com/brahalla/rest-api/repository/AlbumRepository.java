7847fe2a0e921a9d32a706347279fa47e41c975c
Daniel Cottone <daniel.cottone@gmail.com>
2019-03-24 09:50:13 -0500

    upgrading to latest spring boot

--- rest-api/src/main/java/com/brahalla/rest-api/repository/AlbumRepository.java
+++ rest-api/src/main/java/com/brahalla/rest-api/repository/AlbumRepository.java
@@ -5,7 +5,7 @@ import com.brahalla.restapi.domain.entity.QAlbum;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -16,7 +16,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import com.querydsl.core.types.dsl.StringPath;
 
 @RepositoryRestResource(collectionResourceRel = "albums", path = "albums")
-public interface AlbumRepository extends PagingAndSortingRepository<Album, Long>, QueryDslPredicateExecutor<Album>, QuerydslBinderCustomizer<QAlbum> {
+public interface AlbumRepository extends PagingAndSortingRepository<Album, Long>, QuerydslPredicateExecutor<Album>, QuerydslBinderCustomizer<QAlbum> {
 
   @Override
   default void customize(QuerydslBindings bindings, QAlbum album) {
--- rest-api/src/main/java/com/brahalla/rest-api/repository/PhotoRepository.java
+++ rest-api/src/main/java/com/brahalla/rest-api/repository/PhotoRepository.java
@@ -5,7 +5,7 @@ import com.brahalla.restapi.domain.entity.QPhoto;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -16,7 +16,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import com.querydsl.core.types.dsl.StringPath;
 
 @RepositoryRestResource(collectionResourceRel = "photos", path = "photos")
-public interface PhotoRepository extends PagingAndSortingRepository<Photo, Long>, QueryDslPredicateExecutor<Photo>, QuerydslBinderCustomizer<QPhoto> {
+public interface PhotoRepository extends PagingAndSortingRepository<Photo, Long>, QuerydslPredicateExecutor<Photo>, QuerydslBinderCustomizer<QPhoto> {
 
   @Override
   default void customize(QuerydslBindings bindings, QPhoto photo) {
