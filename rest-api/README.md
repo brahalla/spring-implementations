0fafff02e44a9922e15905be6b61fa68ca927a5a
Daniel Cottone <daniel.cottone@gmail.com>
2019-03-24 09:36:25 -0500

    updates

---
+++ rest-api/README.md
@@ -0,0 +1,7 @@
+#rest-api
+
+##Overview
+This is an example implementation of a RESTful CRUD API using Spring Data REST and QueryDSL.
+
+##Build
+To build, use `mvn clean package`. To run, use `mvn spring-boot:run`.
--- rest-api/src/main/java/com/brahalla/rest-api/Application.java
+++ rest-api/src/main/java/com/brahalla/rest-api/Application.java
@@ -2,8 +2,15 @@ package com.brahalla.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@Import({
+	//springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class,
+})
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
